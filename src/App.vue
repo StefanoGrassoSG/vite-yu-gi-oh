b1c3adf8c18045e7368175536e05b89de7724f07
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-07-17 16:45:36 +0200

    ADD axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script >
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
+import axios from 'axios';
 
 export default {
   data() {
@@ -10,6 +11,12 @@ export default {
   },
   methods: {
 
+  },
+  created() {
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+    .then(response => {
+      console.log(response.data)
+    })
   },
   components: {
     HeaderComponent,
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -11,12 +11,20 @@ export default {
 <template>
     <main>
         <div class="container">
-            <select class="form-select w-25" aria-label="Default select example">
-                <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-            </select>
+            <div class="py-4 row">
+                <div class="col-auto">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Alien</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="row">
+                
+            </div>
         </div>
     </main>
 </template>
