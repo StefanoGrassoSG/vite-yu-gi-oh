82ad695fa5ac485383b7d8960182e8ddda8d213a
stefano <127569433+StefanoGrassoSG@users.noreply.github.com>
2023-07-17 15:47:10 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script >
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<template>
+
+  <header>
+    HEADER
+  </header>
+
+  <main>
+    MAIN
+  </main>
+
+  <footer>
+    FOOTER
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+@use "assets/scss/main";
+</style>
